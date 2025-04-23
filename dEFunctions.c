93bf0de986a88c34792507632e50ecd57e72d66e
Karina-Krystal <85716711+Karina-Krystal@users.noreply.github.com>
2025-04-22 17:14:13 -0700

    Update dEFunctions.c

---
+++ dEFunctions.c
@@ -0,0 +1,27 @@
+/**************************************************************
+* Class::  CSC-415-01 Spring 2025
+* Name:: Karina Alvarado Mendoza
+* Student IDs:: 921299233
+* GitHub-Name:: Karina-Krystal
+* Group-Name:: Horse
+* Project:: Basic File System
+*
+* File:: dEFunctions.c
+*
+* Description:: 
+*
+**************************************************************/
+
+#include <mfs.h>
+
+//function for isDir
+int isDir(DirectoryEntry * entry) 
+{
+    return entry->isDir;
+}
+
+//function for isFile, if it returns 0 is file, 1 directory
+int isFile(DirectoryEntry * entry) 
+{
+    return entry->isDir;
+}
